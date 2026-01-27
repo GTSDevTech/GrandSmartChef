495c085933ced19eec7ed6ee567aa8c891ca6876
clase <gtejadosanchez@safareyes.es>
2026-01-27 10:04:55 +0100

    config to upload static files correctly

---
+++ backend/GrandChefSupreme/src/main/java/com/grandchefsupreme/config/StaticResourceConfig.java
@@ -0,0 +1,15 @@
+package com.grandchefsupreme.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticResourceConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/api/uploads/profile/**")
+                .addResourceLocations("file:/data/uploads/profile/");
+    }
+}
--- frontend/GrandSmartChef/src/environments/environment.prod.ts
+++ frontend/GrandSmartChef/src/environments/environment.prod.ts
@@ -9,4 +9,4 @@ export const environment = {
   production: true,
   apiUrl: '/api',
   imageBaseUrl: '/api/uploads/profile/'
-};
+}
