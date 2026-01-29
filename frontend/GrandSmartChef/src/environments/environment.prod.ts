31b2ba1857f3c09d1f63608f5adf2d2f23f4471f
GTS-88 <gtejadosanchez@safareyes.es>
2026-01-29 10:49:51 +0100

    enviroment

--- frontend/GrandSmartChef/src/environments/environment.prod.ts
+++ frontend/GrandSmartChef/src/environments/environment.prod.ts
@@ -1,25 +1,23 @@
 import { Capacitor } from '@capacitor/core';
 
-const API_WEB = '/api';
-const API_RENDER = 'https://grandsmartchef-app.onrender.com/api';
+// const API_WEB = '/api';
+// const API_RENDER = 'https://grandsmartchef-app.onrender.com/api';
+//
+// export const environment = {
+//   production: false,
+//   apiUrl: Capacitor.isNativePlatform() ? API_RENDER : API_WEB,
+//   imageBaseUrl: Capacitor.isNativePlatform()
+//     ? 'https://grandsmartchef-app.onrender.com'
+//     : ''
+// };
+//
 
-export const environment = {
-  production: false,
-  apiUrl: Capacitor.isNativePlatform() ? API_RENDER : API_WEB,
-  imageBaseUrl: Capacitor.isNativePlatform()
-    ? 'https://grandsmartchef-app.onrender.com'
-    : ''
-};
-
-
-
-/*
 export const environment = {
   production: true,
   apiUrl: '/api',
-  imageBaseUrl: 'http://localhost:8080/api/uploads/profile/'
+  imageBaseUrl: '/api/uploads/profile/'
 };
-*/
+
 // export const environment = {
 //   production: true,
 //   apiUrl: '/api',
